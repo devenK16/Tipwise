9d0317198ab73ef7eacfdf03a4cf029ac2e3df16
devenK16 <devenkhobragade007@gmail.com>
2024-05-06 10:26:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/tipwise/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.tipwise.utils
+
+object Constants {
+    const val TAG = "TipwiseApp"
+    const val BASE_URL = "https://192.162.1.6:5000/"
+    const val PREFS_TOKEN_FILE = "PREFS_TOKEN_FILE"
+    const val USER_TOKEN = "USER_TOKEN"
+}
