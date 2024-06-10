e9bcdc9698e4d1fdbc1f536ec17797cd8c577fc7
devenK16 <devenkhobragade007@gmail.com>
2024-06-10 11:09:26 +0530

    add worker screen top bar color fixed

--- app/src/main/java/com/example/tipwise/ui/worker/AddWorkerScreen.kt
+++ app/src/main/java/com/example/tipwise/ui/worker/AddWorkerScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -243,7 +244,10 @@ fun AddWorkerScreen(
                             tint = TipzonnBlack
                         )
                     }
-                }
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White
+                )
             )
         },
         containerColor = Color.White
